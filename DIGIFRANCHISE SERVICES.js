cef1507d372a64d418b108a620920723c7afd214
ruberwa <ruberwa3@gmail.com>
2024-02-25 18:16:31 +0200

    Seed: new digifranchise service

--- DIGIFRANCHISE SERVICES.js
+++ DIGIFRANCHISE SERVICES.js
@@ -15,75 +15,74 @@ const insightfulJourneyServices = [
     { description: 'Mental Health Assessments', unitPrice: '200' },
   ];
   
-  const sitNStayServices = [
-    { description: 'House sitting (without pets)', unitPrice: '250' },
-    { description: 'House sitting (with pets)', unitPrice: '300' },
-    { description: 'Pet sitting', unitPrice: '50' },
-    { description: 'Dog walking', unitPrice: '50' },
+const sitNStayServices = [
+    { serviceName: 'House sitting (without pets)', description: 'House sitting (without pets)', unitPrice: '250' },
+    { serviceName: 'House sitting (with pets)', description: 'House sitting (with pets)', unitPrice: '300' },
+    { serviceName: 'Pet sitting', description: 'Pet sitting', unitPrice: '50' },
+    { serviceName: 'Dog walking', description: 'Dog walking', unitPrice: '50' },
   ];
   
-  const tidyPatchServices = [
-    { description: 'Indoor regular cleaning', unitPrice: '100' },
-    { description: 'Indoor deep cleaning', unitPrice: '250' },
-    { description: 'Outdoor cleaning', unitPrice: '150' },
+const tidyPatchServices = [
+    { serviceName: 'Indoor regular cleaning', description: 'Indoor regular cleaning', unitPrice: '100' },
+    { serviceName: 'Indoor deep cleaning', description: 'Indoor deep cleaning', unitPrice: '250' },
+    { serviceName: 'Outdoor cleaning', description: 'Outdoor cleaning', unitPrice: '150' },
   ];
   
   const flairServices = [
-    { description: 'Fashion consultation', unitPrice: '100' },
-    { description: 'Shopping guide - video call', unitPrice: '50' },
-    { description: 'Shopping guide - in person', unitPrice: '100' }, 
+    { serviceName: 'Fashion consultation', description: 'Fashion consultation', unitPrice: '100' },
+    { serviceName: 'Shopping guide - video call', description: 'Shopping guide - video call', unitPrice: '50' },
+    { serviceName: 'Shopping guide - in person', description: 'Shopping guide - in person', unitPrice: '100' },
   ];
   
   const stitched4UServices = [
-    { description: 'Zip repair and replacement - Trousers and skirts', unitPrice: '100' },
-    { description: 'Zip repair and replacement - Jeans, dresses and short jackets', unitPrice: '110' },
-    { description: 'Zip repair and replacement - Long jackets and cushions', unitPrice: '120' },
-    { description: 'Alterations - Lengthening and shortening', unitPrice: '110' },
-    { description: 'Alterations - Resize up or down', unitPrice: '120' },
-    { description: 'Non-clothing alterations', unitPrice: '120' },
-    { description: 'Patching', unitPrice: '65' },
-    { description: 'Button replacement', unitPrice: '20' },
+    { serviceName: 'Zip repair and replacement - Trousers and skirts', description: 'Zip repair and replacement - Trousers and skirts', unitPrice: '100' },
+    { serviceName: 'Zip repair and replacement - Jeans, dresses and short jackets', description: 'Zip repair and replacement - Jeans, dresses and short jackets', unitPrice: '110' },
+    { serviceName: 'Zip repair and replacement - Long jackets and cushions', description: 'Zip repair and replacement - Long jackets and cushions', unitPrice: '120' },
+    { serviceName: 'Alterations - Lengthening and shortening', description: 'Alterations - Lengthening and shortening', unitPrice: '110' },
+    { serviceName: 'Alterations - Resize up or down', description: 'Alterations - Resize up or down', unitPrice: '120' },
+    { serviceName: 'Non-clothing alterations', description: 'Non-clothing alterations', unitPrice: '120' },
+    { serviceName: 'Patching', description: 'Patching', unitPrice: '65' },
+    { serviceName: 'Button replacement', description: 'Button replacement', unitPrice: '20' },
   ];
   
   const learnerHubServices = [
-    { description: 'Online tutoring sessions', unitPrice: '100' },
-    { description: 'In person tutoring sessions', unitPrice: '150' }, 
+    { serviceName: 'Online tutoring sessions', description: 'Online tutoring sessions', unitPrice: '100' },
+    { serviceName: 'In person tutoring sessions', description: 'In person tutoring sessions', unitPrice: '150' },
   ];
   
   const collectifyServices = [
-    { description: 'Survey design and questionnaire development', unitPrice: '190' },
-    { description: 'Survey execution - digitally', unitPrice: '5' },
-    { description: 'Survey execution - in-person', unitPrice: '50' },
-    { description: 'In Field Data Collection', unitPrice: '200' },
-    { description: 'Data Cleaning and Capturing', unitPrice: '70' },
+    { serviceName: 'Survey design and questionnaire development', description: 'Survey design and questionnaire development', unitPrice: '190' },
+    { serviceName: 'Survey execution - digitally', description: 'Survey execution - digitally', unitPrice: '5' },
+    { serviceName: 'Survey execution - in-person', description: 'Survey execution - in-person', unitPrice: '50' },
+    { serviceName: 'In Field Data Collection', description: 'In Field Data Collection', unitPrice: '200' },
+    { serviceName: 'Data Cleaning and Capturing', description: 'Data Cleaning and Capturing', unitPrice: '70' },
   ];
   
   const beautyAndBestServices = [
-    { description: 'Hairstyling', unitPrice: '15' },
-    { description: 'Nail care', unitPrice: '15' },
-    { description: 'Make-up application', unitPrice: '15' },
-    { description: 'Hair cuts', unitPrice: '15' },
+    { serviceName: 'Hairstyling', description: 'Hairstyling', unitPrice: '15' },
+    { serviceName: 'Nail care', description: 'Nail care', unitPrice: '15' },
+    { serviceName: 'Make-up application', description: 'Make-up application', unitPrice: '15' },
+    { serviceName: 'Hair cuts', description: 'Hair cuts', unitPrice: '15' },
   ];
   
   const cropMinderServices = [
-    { description: 'Site assessment and planning', unitPrice: '120' },
-    { description: 'Crop selection and rotation', unitPrice: '120' },
-    { description: 'Soil health management', unitPrice: '120' },
-    { description: 'Harvesting and post-harvest handling', unitPrice: '120' },
-    { description: 'Pest and disease management', unitPrice: '120' },
-    { description: 'Water management', unitPrice: '120' },
-    { description: 'Crop care and maintenance', unitPrice: '120' },
+    { serviceName: 'Site assessment and planning', description: 'Site assessment and planning', unitPrice: '120' },
+    { serviceName: 'Crop selection and rotation', description: 'Crop selection and rotation', unitPrice: '120' },
+    { serviceName: 'Soil health management', description: 'Soil health management', unitPrice: '120' },
+    { serviceName: 'Harvesting and post-harvest handling', description: 'Harvesting and post-harvest handling', unitPrice: '120' },
+    { serviceName: 'Pest and disease management', description: 'Pest and disease management', unitPrice: '120' },
+    { serviceName: 'Water management', description: 'Water management', unitPrice: '120' },
+    { serviceName: 'Crop care and maintenance', description: 'Crop care and maintenance', unitPrice: '120' },
   ];
   
   const aquaShineServices = [
-    { description: 'Express Exterior Wash - Sedan, Small Bakkie, Hatchback', unitPrice: '70' },
-    { description: 'Express Exterior Wash - SUV', unitPrice: '80' },
-    { description: 'Express Exterior Wash - Big Bakkie', unitPrice: '100' },
-    { description: 'Full Exterior Wash and Interior Detailing - Sedan, Small Bakkie, Hatchback', unitPrice: '200' },
-    { description: 'Full Exterior Wash and Interior Detailing - SUV', unitPrice: '230' },
-    { description: 'Full Exterior Wash and Interior Detailing - Big Bakkie', unitPrice: '250' },
-    { description: 'Basic Exterior and Interior Bundle - Sedan, Small Bakkie, Hatchback', unitPrice: '150' },
-    { description: 'Basic Exterior and Interior Bundle - SUV', unitPrice: '170' },
-    { description: 'Basic Exterior and Interior Bundle - Big Bakkie', unitPrice: '190' },
+    { serviceName: 'Express Exterior Wash - Sedan, Small Bakkie, Hatchback', description: 'Express Exterior Wash - Sedan, Small Bakkie, Hatchback', unitPrice: '70' },
+    { serviceName: 'Express Exterior Wash - SUV', description: 'Express Exterior Wash - SUV', unitPrice: '80' },
+    { serviceName: 'Express Exterior Wash - Big Bakkie', description: 'Express Exterior Wash - Big Bakkie', unitPrice: '100' },
+    { serviceName: 'Full Exterior Wash and Interior Detailing - Sedan, Small Bakkie, Hatchback', description: 'Full Exterior Wash and Interior Detailing - Sedan, Small Bakkie, Hatchback', unitPrice: '200' },
+    { serviceName: 'Full Exterior Wash and Interior Detailing - SUV', description: 'Full Exterior Wash and Interior Detailing - SUV', unitPrice: '230' },
+    { serviceName: 'Full Exterior Wash and Interior Detailing - Big Bakkie', description: 'Full Exterior Wash and Interior Detailing - Big Bakkie', unitPrice: '250' },
+    { serviceName: 'Basic Exterior and Interior Bundle - Sedan, Small Bakkie, Hatchback', description: 'Basic Exterior and Interior Bundle - Sedan, Small Bakkie, Hatchback', unitPrice: '150' },
+    { serviceName: 'Basic Exterior and Interior Bundle - SUV', description: 'Basic Exterior and Interior Bundle - SUV', unitPrice: '170' },
+    { serviceName: 'Basic Exterior and Interior Bundle - Big Bakkie', description: 'Basic Exterior and Interior Bundle - Big Bakkie', unitPrice: '190' },
   ];
-  
