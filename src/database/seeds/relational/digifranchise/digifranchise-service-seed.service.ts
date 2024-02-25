9f340bec6aa24f867b8bcc46d46159acc32563f8
ruberwa <ruberwa3@gmail.com>
2024-02-25 17:35:42 +0200

    seeds: digifranchise service

--- src/database/seeds/relational/digifranchise/digifranchise-service-seed.service.ts
+++ src/database/seeds/relational/digifranchise/digifranchise-service-seed.service.ts
@@ -1,71 +1,80 @@
+
 import { InjectRepository } from "@nestjs/typeorm";
 import { Digifranchise } from "src/digifranchise/entities/digifranchise.entity";
-import { RoleEnum } from "src/roles/roles.enum";
+import { DigifranchiseService } from "src/digifranchise/entities/digifranchise-service.entity"; 
 import { StatusEnum } from "src/statuses/statuses.enum";
-import type { Repository } from "typeorm";
-
+import { Repository } from "typeorm";
 
 export class DigifranchiseSeedService {
-
     constructor(
         @InjectRepository(Digifranchise)
-        private repository: Repository<Digifranchise>,
+        private digifranchiseRepository: Repository<Digifranchise>,
+        @InjectRepository(DigifranchiseService)
+        private serviceRepository: Repository<DigifranchiseService>, 
     ) { }
 
     async run() {
-        const checkBodyRevamp = await this.repository.count({
+        const checkBodyRevamp = await this.digifranchiseRepository.count({
             where: {
                 digifranchiseName: "Body Revamp",
             },
         });
 
-        if (!checkBodyRevamp) {
-            await this.repository.save(
-                this.repository.create({
-                    digifranchiseName: "Body Revamp",
-                    description: "Body Revamp is a personal training digifranchise where certified individuals offer personalized personal training services to their customers. Customers can either subscribe to online exercise and training content or sign up for training sessions delivered live online or in-person.",
-                    status: StatusEnum.active,
-                    services: [
-                        {
-                            serviceName: 'Live Training Sessions 1-on-1',
-                            description: 'Live Training Sessions',
-                            unitPrice: '100',
-                        },
-                        {
-                            serviceName: 'Live Training Sessions 1-on-1',
-                            description: 'Live Training Sessions',
-                            unitPrice: '100'
-                        },
-                        {
-                            serviceName: 'Live Training Sessions Group (2-5)',
-                            description: 'Live Training Sessions',
-                            unitPrice: '50'
-                        },
-                        {
-                            serviceName: 'Personal Training Sessions 1-on-1',
-                            description: 'Personal Training Sessions',
-                            unitPrice: '200'
-                        },
-                        {
-                            serviceName: 'Personal Training Sessions Group (2-5)',
-                            description: 'Personal Training Sessions',
-                            unitPrice: '100'
-                        },
-                        {
-                            serviceName: 'Custom Exercise Plans',
-                            description: 'Custom Exercise Plans',
-                            unitPrice: '250'
-                        },
-                        {
-                            serviceName: 'Exercise Channel Subscription - 30 days',
-                            description: 'Exercise Channel Subscription - 30 days',
-                            unitPrice: '50'
-                        },
+        if (checkBodyRevamp === 0) {
+            const digifranchise = this.digifranchiseRepository.create({
+                digifranchiseName: "Body Revamp",
+                description: "Body Revamp is a personal training digifranchise where certified individuals offer personalized personal training services to their customers. Customers can either subscribe to online exercise and training content or sign up for training sessions delivered live online or in-person.",
+                status: StatusEnum.active,
+            });
 
-                    ]
-                }),
-            );
+            const savedDigifranchise = await this.digifranchiseRepository.save(digifranchise);
+
+            const services = [
+                {
+                    serviceName: 'Live Training Sessions 1-on-1',
+                    description: 'Live Training Sessions',
+                    unitPrice: '100',
+                },
+                {
+                    serviceName: 'Live Training Sessions 1-on-1',
+                    description: 'Live Training Sessions',
+                    unitPrice: '100'
+                },
+                {
+                    serviceName: 'Live Training Sessions Group (2-5)',
+                    description: 'Live Training Sessions',
+                    unitPrice: '50'
+                },
+                {
+                    serviceName: 'Personal Training Sessions 1-on-1',
+                    description: 'Personal Training Sessions',
+                    unitPrice: '200'
+                },
+                {
+                    serviceName: 'Personal Training Sessions Group (2-5)',
+                    description: 'Personal Training Sessions',
+                    unitPrice: '100'
+                },
+                {
+                    serviceName: 'Custom Exercise Plans',
+                    description: 'Custom Exercise Plans',
+                    unitPrice: '250'
+                },
+                {
+                    serviceName: 'Exercise Channel Subscription - 30 days',
+                    description: 'Exercise Channel Subscription - 30 days',
+                    unitPrice: '50'
+                },
+
+            ];
+
+            for (const service of services) {
+                const serviceEntity = this.serviceRepository.create({
+                    ...service,
+                    digifranchise: savedDigifranchise, 
+                });
+                await this.serviceRepository.save(serviceEntity);
+            }
         }
-
     }
 }
