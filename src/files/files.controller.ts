e054831d7b459eeace218e9f941f77b55af29408
Ntare22 <jim.ntare@gmail.com>
2024-03-08 08:35:42 +0300

    create delete file

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ConflictException, Controller, Post, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, ConflictException, Controller, Post, Delete, Param, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
@@ -8,6 +8,7 @@ import { extname } from 'path';
 import { User } from 'src/users/domain/user';
 import { File } from './entities/file.entity';
 import { Repository } from 'typeorm';
+import { FilesService } from './files.service';
 
 @ApiTags('Uploads')
 @Controller({
@@ -16,6 +17,7 @@ import { Repository } from 'typeorm';
 })
 export class FilesController {
   constructor(
+    private readonly fileService: FilesService,
     @InjectRepository(File)
     private readonly filesRepository: Repository<File>,
   ) {}
@@ -54,6 +56,55 @@ export class FilesController {
     },
   }))
   async uploadIdDocument(@UploadedFile() file: Express.Multer.File) {
+    const fileExists = await this.filesRepository.findOne({ where: { filename: file.filename }})
+
+    if (!fileExists) {
+      const newFile = this.filesRepository.create({
+        filename: file.filename,
+        filePath: file.path
+      })
+      const savedFile = await this.filesRepository.save(newFile)
+      return savedFile
+    } else {
+      throw new ConflictException('file already exits')
+    }
+
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/profile/pic')
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads/profile/pic',
+      filename: (req, file, callback) => {
+        const userId = (req.user as User).id;
+        callback(null, `${userId}_profile_pic${extname(file.originalname)}`);
+      }
+    }),
+    limits: {
+      fileSize: 1024 * 1024 * 2,
+    },
+    fileFilter: (req, file, callback) => {
+      if (!file.originalname.match(/\.(pdf|jpeg|jpg|png)$/)) {
+        return callback(new BadRequestException('Unsupported file type'), false);
+      }
+      callback(null, true);
+    },
+  }))
+  async uploadProfilePic(@UploadedFile() file: Express.Multer.File) {
     const fileExists = await this.filesRepository.findOne({ where: { filename: file.filename }})
 
     if (!fileExists) {
@@ -311,5 +362,10 @@ export class FilesController {
     } else {
       throw new ConflictException('file already exits')
     }
+  }
+
+  @Delete(':filename')
+  async deletePicture(@Param('filename') filename: string): Promise<void> {
+    await this.fileService.deletePicture(filename);
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as fs from 'fs';
+import { File } from './entities/file.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  constructor(
+    @InjectRepository(File)
+    private readonly filesRepository: Repository<File>,
+  ) {}
+  async deletePicture(filename: string): Promise<void> {
+    const getFilePath = await this.filesRepository.findOne({ where: { filename }})
+  
+    
+    try {
+      if (!getFilePath) {
+        throw new NotFoundException('File not found')
+      }
+      if (fs.existsSync(getFilePath.filePath)) {
+        fs.unlinkSync(getFilePath.filePath);
+        await this.filesRepository.delete(getFilePath.id)
+      } else {
+        throw new NotFoundException('File not found');
+      }
+    } catch (error) {
+      throw new NotFoundException('File not found');
+    }
+  }
+}
