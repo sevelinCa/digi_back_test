926078e4783e4ad285b4075b38df5711698ce456
ruberwa <ruberwa3@gmail.com>
2024-03-11 08:01:25 +0200

    Updating product seeds

--- src/database/seeds/relational/digifranchise-product/digifranchise-product-seed.service.ts
+++ src/database/seeds/relational/digifranchise-product/digifranchise-product-seed.service.ts
@@ -13,93 +13,35 @@ export class DigifranchiseProductSeedService {
     ) { }
 
     async run() {
-        await this.seedGymMaterial();
-        await this.seedSalonMaterial();
+        await this.seedBodyRevamp();
     }
 
-    private async seedGymMaterial() {
+    private async seedBodyRevamp() {
         const check = await this.digifranchiseRepository.count({
-            where: { digifranchiseName: "GYM Material" },
+            where: { digifranchiseName: "Body Revamp" },
         });
 
         if (check === 0) {
-            const newDigifranchise = this.digifranchiseRepository.create({
-                digifranchiseName: "GYM Material",
-                description: "GYM Material is a personal training digifranchiseId where certified individuals offer personalized personal training services to their customers. Customers can either subscribe to online exercise and training content or sign up for training sessions delivered live online or in-person.",
+            const digifranchiseId = this.digifranchiseRepository.create({
+                digifranchiseName: "Body Revamp",
+                description: "Body Revamp is a personal training digifranchiseId where certified individuals offer personalized personal training services to their customers. Customers can either subscribe to online exercise and training content or sign up for training sessions delivered live online or in-person.",
                 status: StatusEnum.active,
             });
 
-            const savedDigifranchise = await this.digifranchiseRepository.save(newDigifranchise);
+            const savedDigifranchise = await this.digifranchiseRepository.save(digifranchiseId);
 
-            const GymMaterialServices = [
-                {
-                    productName: 'Live Training Sessions 1-on-1',
-                    description: 'Live Training Sessions',
-                    unitPrice: '100',
-                },
-                {
-                    productName: 'Live Training Sessions 1-on-1',
-                    description: 'Live Training Sessions',
-                    unitPrice: '100'
-                },
-                {
-                    productName: 'Live Training Sessions Group (2-5)',
-                    description: 'Live Training Sessions',
-                    unitPrice: '50'
-                },
-                {
-                    productName: 'Personal Training Sessions 1-on-1',
-                    description: 'Personal Training Sessions',
-                    unitPrice: '200'
-                },
-            ];
-
-            for (const service of GymMaterialServices) {
-                const serviceEntity = this.productRepository.create({
-                    ...service,
-                    digifranchiseId: savedDigifranchise, 
-                });
-                await this.productRepository.save(serviceEntity);
-            }
-        }
-    }
-
-    private async seedSalonMaterial() {
-        const check = await this.digifranchiseRepository.count({
-            where: { digifranchiseName: "Salon Material" },
-        });
-
-        if (check === 0) {
-            const newDigifranchise = this.digifranchiseRepository.create({
-                digifranchiseName: "Salon Material",
-                description: "Salon Material offers a range of mental health services, including counseling and therapy sessions designed to support individual and group needs.",
-                status: StatusEnum.active,
-            });
-
-            const savedDigifranchise = await this.digifranchiseRepository.save(newDigifranchise);
-
-            const SalonMaterialServices = [
-                {
-                    productName: '1-on-1 Counselling',
-                    description: '1-on-1 Counselling',
-                    unitPrice: '150'
-                },
-                {
-                    productName: 'Play Therapy for Kids',
-                    description: 'Play Therapy for Kids',
-                    unitPrice: '120'
-                },
+            const bodyRevampServices = [
                 {
                     productName: 'Custom Exercise Plans',
                     description: 'Custom Exercise Plans',
                     unitPrice: '250'
-                },
+                }
             ];
 
-            for (const service of SalonMaterialServices) {
+            for (const service of bodyRevampServices) {
                 const serviceEntity = this.productRepository.create({
                     ...service,
-                    digifranchiseId: savedDigifranchise, 
+                    digifranchiseId: savedDigifranchise,
                 });
                 await this.productRepository.save(serviceEntity);
             }
