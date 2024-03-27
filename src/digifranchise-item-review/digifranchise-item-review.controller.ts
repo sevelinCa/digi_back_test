ce3440c143591de308aa056d643adfc6060ba1d0
ruberwa <ruberwa3@gmail.com>
2024-03-27 07:42:43 +0200

    rating-order table structure

--- src/digifranchise-item-review/digifranchise-item-review.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('digifranchise-item-review')
-export class DigifranchiseItemReviewController {}
---
+++ src/rating-order/rating-order.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('rating-order')
+export class RatingOrderController {}
