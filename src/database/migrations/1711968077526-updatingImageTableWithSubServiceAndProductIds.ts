f324667c17046f4b93a973c6a55f58a5b8ea929b
ruberwa <ruberwa3@gmail.com>
2024-04-01 12:43:00 +0200

    Fixing migrations

--- src/database/migrations/1711968077526-updatingImageTableWithSubServiceAndProductIds.ts
+++ src/database/migrations/1711968077526-updatingImageTableWithSubServiceAndProductIds.ts
@@ -1,22 +1,26 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class UpdatingImageTableWithSubServiceAndProductIds1711905985085 implements MigrationInterface {
-    name = 'UpdatingImageTableWithSubServiceAndProductIds1711905985085'
+export class UpdatingImageTableWithSubServiceAndProductIds1711968077526 implements MigrationInterface {
+    name = 'UpdatingImageTableWithSubServiceAndProductIds1711968077526'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" DROP CONSTRAINT "FK_6c2af3688c5568733fb22cd8342"`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_product" DROP CONSTRAINT "FK_f8dd242ee713e1556c64cbae57a"`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" DROP CONSTRAINT "FK_7744fe7cf2b182d8ec90e05c571"`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" DROP CONSTRAINT "FK_e54de4b7aba4853d8f0c5540b84"`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" DROP CONSTRAINT "FK_b4059529c205d91c7d138685b55"`);
-        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" RENAME COLUMN "digifrachiseServiceId" TO "digifranchiseServiceId"`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_product" RENAME COLUMN "ownedProductId" TO "digifranchiseProductId"`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" RENAME COLUMN "ownedServiceId" TO "digifranchiseServiceId"`);
         await queryRunner.query(`CREATE TABLE "digifranchise_sub_service_category" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "serviceCategoryName" character varying(255) NOT NULL, "unitPrice" character varying(255) NOT NULL, "description" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deleteAt" TIMESTAMP, "subServiceId" uuid, CONSTRAINT "PK_452b4401dc0cbc16524e5c1db24" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" DROP COLUMN "digifranchiseId"`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" DROP COLUMN "digifranchiseOwnerId"`);
         await queryRunner.query(`ALTER TABLE "digifranchise_gallery_image" ADD "subServiceId" uuid`);
         await queryRunner.query(`ALTER TABLE "digifranchise_gallery_image" ADD "subProductId" uuid`);
+        await queryRunner.query(`ALTER TABLE "order_table" ADD "orderAdditionalInfo" json`);
         await queryRunner.query(`ALTER TABLE "order_table" ADD "subProduct" uuid`);
         await queryRunner.query(`ALTER TABLE "order_table" ADD "subService" uuid`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD "digifranchiseOwnerId" uuid`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD "digifranchiseId" uuid`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_product" ADD CONSTRAINT "FK_60e78e864a4429c58348e3a2845" FOREIGN KEY ("digifranchiseProductId") REFERENCES "digifranchise_product"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "digifranchise_sub_service_category" ADD CONSTRAINT "FK_5d9c767ba9555106fa4f3987e43" FOREIGN KEY ("subServiceId") REFERENCES "digifranchise_sub_services"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" ADD CONSTRAINT "FK_df1712e88c792388ca3f60e14d3" FOREIGN KEY ("digifranchiseServiceId") REFERENCES "digifranchise_service_offered"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "digifranchise_gallery_image" ADD CONSTRAINT "FK_ca0bd1fb94e3f5a14c72bdcb7d2" FOREIGN KEY ("subServiceId") REFERENCES "digifranchise_sub_services"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -36,19 +40,23 @@ export class UpdatingImageTableWithSubServiceAndProductIds1711905985085 implemen
         await queryRunner.query(`ALTER TABLE "digifranchise_gallery_image" DROP CONSTRAINT "FK_ca0bd1fb94e3f5a14c72bdcb7d2"`);
         await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" DROP CONSTRAINT "FK_df1712e88c792388ca3f60e14d3"`);
         await queryRunner.query(`ALTER TABLE "digifranchise_sub_service_category" DROP CONSTRAINT "FK_5d9c767ba9555106fa4f3987e43"`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_product" DROP CONSTRAINT "FK_60e78e864a4429c58348e3a2845"`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" DROP COLUMN "digifranchiseId"`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" DROP COLUMN "digifranchiseOwnerId"`);
         await queryRunner.query(`ALTER TABLE "order_table" DROP COLUMN "subService"`);
         await queryRunner.query(`ALTER TABLE "order_table" DROP COLUMN "subProduct"`);
+        await queryRunner.query(`ALTER TABLE "order_table" DROP COLUMN "orderAdditionalInfo"`);
         await queryRunner.query(`ALTER TABLE "digifranchise_gallery_image" DROP COLUMN "subProductId"`);
         await queryRunner.query(`ALTER TABLE "digifranchise_gallery_image" DROP COLUMN "subServiceId"`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD "digifranchiseOwnerId" uuid`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD "digifranchiseId" uuid`);
         await queryRunner.query(`DROP TABLE "digifranchise_sub_service_category"`);
-        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" RENAME COLUMN "digifranchiseServiceId" TO "digifrachiseServiceId"`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" RENAME COLUMN "digifranchiseServiceId" TO "ownedServiceId"`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_product" RENAME COLUMN "digifranchiseProductId" TO "ownedProductId"`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD CONSTRAINT "FK_b4059529c205d91c7d138685b55" FOREIGN KEY ("digifranchiseOwnerId") REFERENCES "digifranchise_owner"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD CONSTRAINT "FK_e54de4b7aba4853d8f0c5540b84" FOREIGN KEY ("digifranchiseId") REFERENCES "digifranchise"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" ADD CONSTRAINT "FK_6c2af3688c5568733fb22cd8342" FOREIGN KEY ("digifrachiseServiceId") REFERENCES "digifranchise_service_offered"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_services" ADD CONSTRAINT "FK_7744fe7cf2b182d8ec90e05c571" FOREIGN KEY ("ownedServiceId") REFERENCES "digifranchise_owned_service_offered"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "digifranchise_sub_product" ADD CONSTRAINT "FK_f8dd242ee713e1556c64cbae57a" FOREIGN KEY ("ownedProductId") REFERENCES "digifranchise_owned_product"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
 }
