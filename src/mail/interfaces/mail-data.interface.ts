eced6e6c83d8685dd994b29adde159d6db887083
ruberwa <ruberwa3@gmail.com>
2024-04-09 08:37:32 +0200

    forgot passwowrd for webs

--- src/mail/interfaces/mail-data.interface.ts
+++ src/mail/interfaces/mail-data.interface.ts
@@ -1,4 +1,10 @@
-export interface MailData<T = never> {
+// export interface MailData<T = never> {
+//   to: string;
+//   data: T;
+// }
+
+
+export interface MailData<T extends { hash: string; websiteUrl?: string; }> {
   to: string;
   data: T;
-}
+ }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -114,4 +114,55 @@ export class MailService {
       },
     });
   }
+
+  async forgotPasswordForWebs(mailData: MailData<{ hash: string, websiteUrl: string }>): Promise<void> {
+
+    const i18n = I18nContext.current();
+    let resetPasswordTitle: MaybeType<string>;
+    let text1: MaybeType<string>;
+    let text2: MaybeType<string>;
+    let text3: MaybeType<string>;
+    let text4: MaybeType<string>;
+
+    if (i18n) {
+      [resetPasswordTitle, text1, text2, text3, text4] = await Promise.all([
+        i18n.t('common.resetPassword'),
+        i18n.t('reset-password.text1'),
+        i18n.t('reset-password.text2'),
+        i18n.t('reset-password.text3'),
+        i18n.t('reset-password.text4'),
+      ]);
+    }
+
+    const url = new URL(mailData.data.websiteUrl + '/password-change');
+    url.searchParams.set('hash', mailData.data.hash);;
+
+    await this.mailerService.sendMail({
+      to: mailData.to,
+      subject: resetPasswordTitle,
+      text: `${url.toString()} ${resetPasswordTitle}`,
+      templatePath: path.join(
+        this.configService.getOrThrow('app.workingDirectory', {
+          infer: true,
+        }),
+        'src',
+        'mail',
+        'mail-templates',
+        'reset-password.hbs',
+      ),
+      context: {
+        title: resetPasswordTitle,
+        url: url.toString(),
+        actionTitle: resetPasswordTitle,
+        app_name: this.configService.get('app.name', {
+          infer: true,
+        }),
+        text1,
+        text2,
+        text3,
+        text4,
+      },
+    });
+  }
+
 }
