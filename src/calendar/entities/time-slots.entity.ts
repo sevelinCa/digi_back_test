ba7f105cb15008df01d74d29f4ed019b0a6c41c8
Ntare22 <jim.ntare@gmail.com>
2024-04-26 15:16:20 +0300

    deploy

---
+++ src/calendar/entities/time-slots.entity.ts
@@ -0,0 +1,39 @@
+// import { DigifranchiseOwner } from "src/digifranchise/entities/digifranchise-ownership.entity";
+// import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+// @Entity()
+// export class AvailabilityTimeSlots {
+//   @PrimaryGeneratedColumn('uuid')
+//     id: string;
+
+//     @ManyToOne(() => DigifranchiseOwner, ownedFranchise => ownedFranchise.availability)
+//     @JoinColumn({ name: 'ownedDigifranchise' })
+//     ownedDigifranchise: DigifranchiseOwner | null;
+
+//     @Column({ type: 'boolean', default: false })
+//     isSlotBooked: boolean;
+
+//     @Column({ type: 'boolean', default: true })
+//     isSlotAvailable: boolean;
+
+//     @Column({ type: 'timestamp', nullable: true })
+//     workingDate?: Date;
+
+//     @Column({ type: 'varchar', length: 255 })
+//     day: string;
+
+//     @Column({ type: 'time' })
+//     startTime: string;
+
+//     @Column({ type: 'time' })
+//     endTime: string;
+
+//     @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+//     createdAt: Date;
+
+//     @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+//     updatedAt: Date;
+
+//     @Column({ type: 'timestamp', nullable: true })
+//     deleteAt: Date | null;
+// }
--- src/database/migrations/1714086388759-updateBookedSloteTable.ts
+++ src/database/migrations/1714086388759-updateBookedSloteTable.ts
@@ -13,17 +13,17 @@ export class UpdateBookedSloteTable1714086388759 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD CONSTRAINT "FK_8045b1f51750c77a13d632d35af" FOREIGN KEY ("ownedDigifranchise") REFERENCES "digifranchise_owner"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
 
         // Check if the constraint exists before adding it
-        await queryRunner.query(`
-            DO $$ BEGIN
-                IF NOT EXISTS (
-                    SELECT 1
-                    FROM   information_schema.table_constraints 
-                    WHERE constraint_name = 'FK_89b14ce607290844cbfb4daa20a'
-                    ) THEN
-                    ALTER TABLE "availability_booked_slots" ADD CONSTRAINT "FK_89b14ce607290844cbfb4daa20a" FOREIGN KEY ("slotId") REFERENCES "availability_slots_time_one_one"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-                END IF;
-            END $$;
-        `);
+        // await queryRunner.query(`
+        //     DO $$ BEGIN
+        //         IF NOT EXISTS (
+        //             SELECT 1
+        //             FROM   information_schema.table_constraints 
+        //             WHERE constraint_name = 'FK_89b14ce607290844cbfb4daa20a'
+        //             ) THEN
+        //             ALTER TABLE "availability_booked_slots" ADD CONSTRAINT "FK_89b14ce607290844cbfb4daa20a" FOREIGN KEY ("slotId") REFERENCES "availability_slots_time_one_one"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+        //         END IF;
+        //     END $$;
+        // `);
 
         await queryRunner.query(`ALTER TABLE "customer_subscription" ADD CONSTRAINT "FK_b4059529c205d91c7d138685b55" FOREIGN KEY ("digifranchiseOwnerId") REFERENCES "digifranchise_owner"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
