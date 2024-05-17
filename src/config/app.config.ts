aace8dd5e96a25e67f7352fb4c18399738033bdc
ruberwa <ruberwa3@gmail.com>
2024-05-17 08:13:54 +0200

    Putting Academy APIs in .env

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -47,24 +47,41 @@ class EnvironmentVariablesValidator {
   @IsString()
   @IsOptional()
   APP_HEADER_LANGUAGE: string;
+
+  @IsString()
+  @IsOptional()
+  ACADEMY_API_BASE_URL: string;
+
+  @IsString()
+  @IsOptional()
+  ACADEMY_CSRF_TOKEN_ENDPOINT: string;
+
+  @IsString()
+  @IsOptional()
+  ACADEMY_REGISTER_ENDPOINT: string;
 }
 
 export default registerAs<AppConfig>('app', () => {
   validateConfig(process.env, EnvironmentVariablesValidator);
 
-  return {
+  const config = {
     nodeEnv: process.env.NODE_ENV || 'development',
     name: process.env.APP_NAME || 'app',
     workingDirectory: process.env.PWD || process.cwd(),
     frontendDomain: process.env.FRONTEND_DOMAIN,
-    backendDomain: process.env.BACKEND_DOMAIN ?? 'http://localhost',
+    backendDomain: process.env.BACKEND_DOMAIN?? 'http://localhost',
     port: process.env.APP_PORT
-      ? parseInt(process.env.APP_PORT, 10)
+     ? parseInt(process.env.APP_PORT, 10)
       : process.env.PORT
-        ? parseInt(process.env.PORT, 10)
+       ? parseInt(process.env.PORT, 10)
         : 3000,
     apiPrefix: process.env.API_PREFIX || 'api',
     fallbackLanguage: process.env.APP_FALLBACK_LANGUAGE || 'en',
     headerLanguage: process.env.APP_HEADER_LANGUAGE || 'x-custom-lang',
+    ACADEMY_API_BASE_URL: process.env.ACADEMY_API_BASE_URL,
+    ACADEMY_CSRF_TOKEN_ENDPOINT: process.env.ACADEMY_CSRF_TOKEN_ENDPOINT,
+    ACADEMY_REGISTER_ENDPOINT: process.env.ACADEMY_REGISTER_ENDPOINT,
   };
+
+  return config;
 });
--- src/config/config.type.ts
+++ src/config/config.type.ts
@@ -1,4 +1,4 @@
-import { AppConfig } from './app-config.type';
+import { AppConfig,  AppConfigType } from './app-config.type';
 // import { AppleConfig } from '../auth-apple/config/apple-config.type';
 import { AuthConfig } from '../auth/config/auth-config.type';
 import { DatabaseConfig } from '../database/config/database-config.type';
@@ -8,7 +8,7 @@ import { DatabaseConfig } from '../database/config/database-config.type';
 import { MailConfig } from '../mail/config/mail-config.type';
 // import { TwitterConfig } from '../auth-twitter/config/twitter-config.type';
 
-export type AllConfigType = {
+export interface AllConfigType extends AppConfigType {
   app: AppConfig;
   // apple: AppleConfig;
   auth: AuthConfig;
